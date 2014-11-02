995ab41b7603d08f6e0e760d7c9c4487330a2e8f
treken <chupekevin@gmail.com>
2014-11-02 17:09:24 -0600

    Update: bring current with Carbon
    Change-Id: I8400f36526c92656f3d619b1eca0b4ac1225ee33

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -587,11 +587,11 @@ public class Status extends PreferenceActivity {
         return Build.SERIAL;
     }
 
-    private String getSarValues() {
-        String headLevel = String.format(getResources().getString(R.string.maximum_head_level,
-                getResources().getString(R.string.sar_head_level)));
-        String bodyLevel = String.format(getResources().getString(R.string.maximum_body_level,
-                getResources().getString(R.string.sar_body_level)));
+    public static String getSarValues(Resources res) {
+        String headLevel = String.format(res.getString(R.string.maximum_head_level,
+                res.getString(R.string.sar_head_level)));
+        String bodyLevel = String.format(res.getString(R.string.maximum_body_level,
+                res.getString(R.string.sar_body_level)));
         return headLevel + "\n" + bodyLevel;
     }
 }
