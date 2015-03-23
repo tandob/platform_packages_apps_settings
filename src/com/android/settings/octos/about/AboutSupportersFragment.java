3c09d6eba064d25fa1a4b5cde78e17aa3f1a09c7
hedwig34 <hedwig34@gmail.com>
2015-03-23 12:32:29 -0400

    Tentacles: Add a default supporters page to display if the user is not online.
    Change-Id: Ic5921791f5e8e15d8793f3d173d0806fee839535

--- src/com/android/settings/octos/about/AboutSupportersFragment.java
+++ src/com/android/settings/octos/about/AboutSupportersFragment.java
@@ -1,6 +1,9 @@
 package com.android.settings.octos.about;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,14 +17,28 @@ public class AboutSupportersFragment extends Fragment {
         // empty fragment constructor
     }
 
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getActivity()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo i = cm.getActiveNetworkInfo();
+        if ((i == null) || (!i.isConnected())) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_about_octos_supporters, container, false);
 
         WebView browser = (WebView) root.findViewById(R.id.octos_supporters_display);
         browser.getSettings().setUserAgentString("Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
-        browser.loadUrl(getString(R.string.octos_supporters_url));
-
+        if (isOnline()) {
+            browser.loadUrl(getString(R.string.octos_supporters_url));
+        } else {
+            browser.loadUrl("file:///android_asset/default_about.html");
+        }
         return root;
     }
 }
