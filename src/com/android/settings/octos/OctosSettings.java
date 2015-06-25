b9b484a7b85e4016a315717803b4d08ae1c97647
hedwig34 <hedwig34@gmail.com>
2015-06-25 16:54:56 -0400

    Tentacles: Add option to hide the Tentacles icon form the launcher.
    Change-Id: I59f0a25736abc7ae2ef63fb6a19b5381644fbf21

--- src/com/android/settings/octos/OctosSettings.java
+++ src/com/android/settings/octos/OctosSettings.java
@@ -16,19 +16,31 @@
 
 package com.android.settings.octos;
 
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
 
-public class OctosSettings extends SettingsPreferenceFragment {
+public class OctosSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_HIDE_TENTACLE_ICON = "hide_tentacles_icon";
+
+    private SwitchPreference mHideTentaclesIcon;
 
     @Override
     protected int getMetricsCategory() {
@@ -40,5 +52,32 @@ public class OctosSettings extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.octos_settings);
+
+        mHideTentaclesIcon = (SwitchPreference) findPreference(KEY_HIDE_TENTACLE_ICON);
+        mHideTentaclesIcon.setOnPreferenceChangeListener(this);
     }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        // If we didn't handle it, let preferences handle it.
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        if (KEY_HIDE_TENTACLE_ICON.equals(key)) {
+            if ((Boolean) objValue) {
+            PackageManager p=this.getPackageManager();
+            p.setComponentEnabledSetting(new ComponentName("com.android.settings","com.android.settings.Settings$OctosSettingsActivity"), PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
+
+            } else {
+            PackageManager p = getPackageManager();
+            ComponentName componentName = new ComponentName("com.android.settings","com.android.settings.Settings$OctosSettingsActivity");
+            p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+            }
+        }
+        return true;
+    }
+
 }
