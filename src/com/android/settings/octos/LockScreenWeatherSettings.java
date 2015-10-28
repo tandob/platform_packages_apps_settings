voidz777/android_packages_apps_Settings

Lock screen: Weather panel improvements, (2/2)
Change-Id: I9588769b26b0b797c3f19a7e0887fbf8238588cc

src/com/android/settings/euphoria/LockScreenWeatherSettings.java
@@ -1,0 +1,80 @@
+/* 
+ * Copyright (C) 2014 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.euphoria;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_CONDITION_ICON =
+            "weather_condition_icon";
+
+    private static final int MONOCHROME_ICON = 0;
+
+    private ListPreference mConditionIcon;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lock_screen_weather_settings);
+        mResolver = getActivity().getContentResolver();
+
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+
+        mConditionIcon =
+                (ListPreference) findPreference(PREF_CONDITION_ICON);
+        mConditionIcon.setValue(String.valueOf(conditionIcon));
+        mConditionIcon.setSummary(mConditionIcon.getEntry());
+        mConditionIcon.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        if (preference == mConditionIcon) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mConditionIcon.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+            refreshSettings();
+            return true;
+        }
+        return false;
+    }
+}

tandob/platform_packages_apps_settings

Change-Id: I35d2461bae2b893ddc82ef46275e0856238c45ab

src/com/android/settings/octos/LockScreenWeatherSettings.java
@@ -1,0 +1,87 @@
+/* 
+ * Copyright (C) 2014 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.octos;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_CONDITION_ICON =
+            "weather_condition_icon";
+
+    private static final int MONOCHROME_ICON = 0;
+
+    private ListPreference mConditionIcon;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.LOCKSCREEN_WEATHER_SETTINGS;
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lock_screen_weather_settings);
+        mResolver = getActivity().getContentResolver();
+
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+
+        mConditionIcon =
+                (ListPreference) findPreference(PREF_CONDITION_ICON);
+        mConditionIcon.setValue(String.valueOf(conditionIcon));
+        mConditionIcon.setSummary(mConditionIcon.getEntry());
+        mConditionIcon.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        if (preference == mConditionIcon) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mConditionIcon.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+            refreshSettings();
+            return true;
+        }
+        return false;
+    }
+}